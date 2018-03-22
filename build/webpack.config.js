6d0c1938f0276997a34220dc3d1629de7087983f
zhangpeipei <peterzhang@PeterdeMBP.lan>
2018-03-22 16:42:19 +0800

    button

---
+++ build/webpack.config.js
@@ -0,0 +1,44 @@
+/**
+ * Created by peterzhang on 2018/3/22.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+console.log(path.resolve(__dirname, '../docs'))
+const config = {
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../docs'),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, '../docs')
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'less-loader'
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+            use: [
+                'file-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'button.html',
+            template: path.resolve(__dirname, '../src/widget/button.html')
+        })
+    ]
+}
+
+module.exports = config;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by peterzhang on 2018/3/22.
+ */
+import './example.less'
